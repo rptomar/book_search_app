9e0c7ddac89510400992d7402fe51bdf1a595de3
rptomar <rohitpal23478@gmail.com>
2022-12-27 05:14:38 -0800

    files are added

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="navbar">
+      <h1 className="header">BOOK SEARCH</h1>
+    </div>
+    <div className="footer">
+      <input  className="searchbar" type="search" placeholder="Search for a book" />
+    </div>
+    </>
+  );
+}
+
+export default App;
